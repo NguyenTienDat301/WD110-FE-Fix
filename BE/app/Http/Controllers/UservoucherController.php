e4a89cd6322161143a771eed85f3dd0c30672b92
NguyenTienDat301 <nguyentiendat.3010204@email.com>
2025-09-14 00:41:02 +0700

    voucher update

--- BE/app/Http/Controllers/UservoucherController.php
+++ BE/app/Http/Controllers/UservoucherController.php
@@ -10,28 +10,36 @@ class UservoucherController extends Controller
 {
     public function index()
     {
+        $search = request('search');
         $userId = Auth::id();
-        $now = now();
-        // Voucher có thể sử dụng: đang active, chưa dùng, còn hạn
+        $now = \Carbon\Carbon::now();
+
         $usableVouchers = Voucher::where('is_active', 1)
             ->where('start_day', '<=', $now)
             ->where('end_day', '>=', $now)
+            ->whereRaw('quantity > used_times')
             ->whereDoesntHave('voucherUsages', function ($query) use ($userId) {
                 $query->where('user_id', $userId);
-            })
-            ->paginate(8);
+            });
+        if ($search) {
+            $usableVouchers = $usableVouchers->where('code', 'like', "%$search%");
+        }
+        $usableVouchers = $usableVouchers->paginate(8);
 
-        // Voucher không thể sử dụng: đã dùng, hết hạn, không active
         $unusableVouchers = Voucher::where(function($query) use ($userId, $now) {
                 $query->where('is_active', 0)
                     ->orWhere('end_day', '<', $now)
+                    ->orWhereRaw('quantity <= used_times')
                     ->orWhereHas('voucherUsages', function ($q) use ($userId) {
                         $q->where('user_id', $userId);
                     });
-            })
-            ->paginate(8);
+            });
+        if ($search) {
+            $unusableVouchers = $unusableVouchers->where('code', 'like', "%$search%");
+        }
+        $unusableVouchers = $unusableVouchers->paginate(8);
 
-        return view('user.voucher', compact('usableVouchers', 'unusableVouchers'));
+        return view('user.voucher', compact('usableVouchers', 'unusableVouchers', 'search'));
     }
 
 
